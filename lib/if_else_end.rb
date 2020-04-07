e58c249e4973018419dde309261e6220a52ca75f
Jesse Langley <jlangleyiii@aol.com>
2020-04-07 21:48:27 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,5 +1,4 @@
-# First a question
-puts "I would really like to know if the current second since January 1st, 1970 is even or odd"
+
 current_time = Time.now 
 current_time = current_time.to_i 
  if current_time % 2 == 0 
