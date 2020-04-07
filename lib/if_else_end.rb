426322d1cf9bbc9136ddf9965289659516c3d5c2
Jesse Langley <jlangleyiii@aol.com>
2020-04-07 21:31:29 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -2,5 +2,5 @@
 Puts "I would really like to know if the current second since January 1st, 1970 is either even or odd"
 Current_time = Time.now 
 Current_time = Current_time.to_i 
-puts "Even!" if Current_time.to_i == even 
-else
+ if Current_time.to_i == even 
+puts "Even!"else
